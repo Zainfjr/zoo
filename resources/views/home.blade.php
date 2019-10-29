8bf204b6136178472fecab0adccc899b20cc91a5
Zainfjr <znfjr05@gmail.com>
2019-10-29 22:25:39 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+<head>
+  <title>Mamang Garox</title>
+  @include('Template.head')
+</head>
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper" id="app">
+
+  <!-- Navbar -->
+  @include('Template.navbar')
+  <!-- /.navbar -->
+
+
+  <!-- Main Sidebar Container -->
+  @include('Template.sidebar-left')
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Starter Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+
+    <!-- Main content -->
+    <div class="content">
+      <!-- ISI HALAMAN -->
+    </div>
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <!-- Main Footer -->
+  @include('Template.footer')
+</div>
+<!-- ./wrapper -->
+
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+@include('Template.script')
+
+</body>
+</html>
